8f2a8387eeeafeb2bcec2b5115e1d25d09d378d4
hyunjun <qfdsf648@naver.com>
2021-07-21 23:54:07 -0700

    copy

--- test.c
+++ test.c
@@ -1,30 +1,69 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int main() {
-
-	int a,b,c,d;
-	printf("31게임하기\n");
-
-
-	for(int i=0; i>31; i++) {
-
-		scanf("%d",&a);
-		scanf("%d",&b);
-
-		i = a+b;
-	        c = a+a;
-		d = b+b;	
+int main(void)
+{
+	int player = 0, computer = 0;
+	int num = 0;
+	int brea=0;
+	srand((unsigned)time(NULL));
 
 
-	if (i>31) {
+	printf("┌──────────────────┐\n");
+	printf("│  배스킨라빈스31 게임 │\n");
+	printf("└──────────────────┘\n\n");
+	printf(" GAME START!\n");
+	printf("※ 1부터 3까지의 수를 입력하시오.\n");
 
-		if (c>d) {
 
-			printf("A win\n");
+	while (1)
+	{
+		printf("\n[사용자] 몇 개의 수를 입력하겠습니까? : ");
+		scanf("%d", &player);
+
+		if (player > 3 || player <= 0)
+		{
+			printf("※ 1부터 3까지의 숫자만 입력하시오\n\n");
+			continue;
 		}
-		else {
-			printf("B win\n");
+
+		for (int i = 1; i <= player; i++)
+		{
+			num++;
+			if (num <= 31)
+			{
+				printf("%d ", num);
+			}
+		}
+			if (num >= 31)
+			{
+				printf("\n★★★ computer win! ★★★\n");
+				brea = 1;
+				break;
+			}
+
+			computer = (rand() % 3 + 1);
+			printf("\n[컴퓨터] :  %d\n", computer);
+
+			for (int i = 0; i < computer; i++)
+			{
+				num++;
+				if (num <= 31)
+				{
+					printf("%d ", num);
+				}
+			}
+				if (num >= 31)
+				{
+					printf("\n★★★ player win! ★★★\n");
+					brea = 1;
+					break;
+				}
+		if (brea == 1)
+		{
+			break;
 		}
 	}
-    }
+	return 0;
 }
